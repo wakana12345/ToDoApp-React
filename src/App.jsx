b819621560ca7f4140d4c5030542af332ada150b
wakana12345 <34619210+wakana12345@users.noreply.github.com>
2026-05-20 20:46:15 +0900

    Hook化 ロジック部分を分けた

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,22 @@
 import "./App.css";
-import { useState } from "react";
-import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "./constants/data";
 
 /*compornents*/
 import { AddTodo } from "./components/AddTodo";
 import { TodoList } from "./components/TodoList";
 
+/* hooks */
+import { useApp } from "./hooks/useApp";
+
 export const App = () => {
-  /* 状態管理変数の宣言 */
-  //TodoList
-  const [todoList, setTodoList] = useState(INIT_TODO_LIST);
-
-  // 入力時データ
-  const [addInputValue, setAddInputValue] = useState("");
-
-  //採番用ID
-  const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
-
-  /* イベント発生時に実行される関数 */
-  //入力データの更新
-  const onChangeAddInputValue = (event) => {
-    setAddInputValue(event.target.value);
-  };
-
-  // add ToDo
-  const handleAddTodo = (event) => {
-    //エンターキーを押されてかつ、空文字じゃないとき
-    if (event.key === "Enter" && addInputValue !== "") {
-      const newUniqueId = uniqueId + 1;
-      const newTodoList = [
-        ...todoList,
-        {
-          id: newUniqueId,
-          title: addInputValue,
-        },
-      ];
-      setTodoList(newTodoList);
-      setUniqueId(newUniqueId);
-      setAddInputValue("");
-    }
-  };
-
-  //Delete ToDo
-  const handleDeleteTodo = (targetId, targetTitle) => {
-    if (window.confirm(`${targetTitle}を削除しますか？`)) {
-      const newTodoList = todoList.filter((todo) => {
-        return todo.id !== targetId;
-      });
-      setTodoList(newTodoList);
-    }
-  };
+  /* hooks */
+  const [state, action] = useApp();
+  // const {
+  //   addInputValue,
+  //   onChangeAddInputValue,
+  //   handleAddTodo,
+  //   todoList,
+  //   handleDeleteTodo,
+  // } = useApp();
 
   return (
     <div className="App">
@@ -58,15 +25,23 @@ export const App = () => {
       {/* Todo追加領域 */}
       <section className="common-area">
         <AddTodo
+          addInputValue={state.addInputValue}
+          onChangeAddInputValue={action.onChangeAddInputValue}
+          handleAddTodo={action.handleAddTodo}
+        />
+        {/* <AddTodo
           addInputValue={addInputValue}
           onChangeAddInputValue={onChangeAddInputValue}
           handleAddTodo={handleAddTodo}
-        />
+        /> */}
       </section>
 
       {/* Todo一覧表示 */}
       <section className="common-area">
-        <TodoList todoList={todoList} handleDeleteTodo={handleDeleteTodo} />
+        <TodoList
+          todoList={state.todoList}
+          handleDeleteTodo={action.handleDeleteTodo}
+        />
       </section>
     </div>
   );
---
+++ src/hooks/useApp.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { INIT_TODO_LIST, INIT_UNIQUE_ID } from "../constants/data";
+
+export const useApp = () => {
+  /* 状態管理変数の宣言 */
+  //TodoList
+  const [todoList, setTodoList] = useState(INIT_TODO_LIST);
+
+  // 入力時データ
+  const [addInputValue, setAddInputValue] = useState("");
+
+  //採番用ID
+  const [uniqueId, setUniqueId] = useState(INIT_UNIQUE_ID);
+
+  /* イベント発生時に実行される関数 */
+  //入力データの更新
+  const onChangeAddInputValue = (event) => {
+    setAddInputValue(event.target.value);
+  };
+
+  // add ToDo
+  const handleAddTodo = (event) => {
+    //エンターキーを押されてかつ、空文字じゃないとき
+    if (event.key === "Enter" && addInputValue !== "") {
+      const newUniqueId = uniqueId + 1;
+      const newTodoList = [
+        ...todoList,
+        {
+          id: newUniqueId,
+          title: addInputValue,
+        },
+      ];
+      setTodoList(newTodoList);
+      setUniqueId(newUniqueId);
+      setAddInputValue("");
+    }
+  };
+
+  //Delete ToDo
+  const handleDeleteTodo = (targetId, targetTitle) => {
+    if (window.confirm(`${targetTitle}を削除しますか？`)) {
+      const newTodoList = todoList.filter((todo) => {
+        return todo.id !== targetId;
+      });
+      setTodoList(newTodoList);
+    }
+  };
+
+  return [
+    {
+      todoList,
+      addInputValue,
+    },
+    {
+      onChangeAddInputValue,
+      handleAddTodo,
+      handleDeleteTodo,
+    },
+  ];
+};
